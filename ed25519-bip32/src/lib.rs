69d453523e0da9302925eba658266b9d4494b87a
Lane Rettig <lanerettig@gmail.com>
2023-05-25 16:30:36 -0400

    Cleanup ed25519-bip32 code
    Use more idiomatic Rust, similar to changes made previously in
    remote-wallet crate

--- ed25519-bip32/src/lib.rs
+++ ed25519-bip32/src/lib.rs
@@ -3,7 +3,6 @@ extern crate wasm_bindgen;
 
 use std::ffi::{c_char, CStr};
 use ed25519_dalek_bip32::{ed25519_dalek::{Keypair, KEYPAIR_LENGTH, SECRET_KEY_LENGTH}, DerivationPath, ExtendedSecretKey};
-use spacemesh_sdkutils::{check_err, err};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -36,17 +35,35 @@ pub extern "C" fn derive_c(
     derivation_path_ptr: *const c_char,
     result: *mut u8,
 ) -> u16 {
+    match _derive_c(seed, seedlen, derivation_path_ptr) {
+        Ok(keypair) => {
+            let result_slice = unsafe { std::slice::from_raw_parts_mut(result, KEYPAIR_LENGTH) };
+            result_slice.copy_from_slice(&keypair.to_bytes());
+            0
+        }
+        Err(err) => {
+            eprintln!("{}", err);
+            1
+        }
+    }
+}
+
+fn _derive_c(
+    seed: *const u8,
+    seedlen: usize,
+    derivation_path_ptr: *const c_char,
+) -> Result<Keypair, Box<dyn std::error::Error>> {
     // Seed must be at least 32 bytes
     if seedlen < SECRET_KEY_LENGTH {
-        err!("seed must be at least 32 bytes");
+        return Err("seed must be at least 32 bytes".into());
     }
     let seed_slice = unsafe { std::slice::from_raw_parts(seed, seedlen) };
     let derivation_path_str = unsafe { CStr::from_ptr(derivation_path_ptr) };
-    let derivation_path_str = derivation_path_str.to_str();
-    check_err!(derivation_path_str, "failed to convert path string from raw parts");
-    let derivation_path_str = derivation_path_str.unwrap().parse();
-    check_err!(derivation_path_str, "failed to parse derivation path");
-    let derivation_path: DerivationPath = derivation_path_str.unwrap();
+    let derivation_path: DerivationPath = derivation_path_str
+        .to_str()
+        .map_err(|e| format!("failed to convert path string to str: {}", e))?
+        .parse()
+        .map_err(|e| format!("failed to parse derivation path: {}", e))?;
 
     // for now we are rather strict with which types of paths we accept, to avoid errors and to
     // be as compatible as possible with BIP-44. the path must be of the format
@@ -54,30 +71,27 @@ pub extern "C" fn derive_c(
     // 540 and all path elements must be hardened. we expect it to contain between 2 and 5
     // elements.
     if derivation_path.path().len() < 2 {
-        err!("path too short");
+        return Err("path too short".into());
     }
     if derivation_path.path().len() > 5 {
-        err!("path too long");
+        return Err("path too long".into());
     }
     if derivation_path.path()[0].to_u32() != 44 {
-        err!("bad path purpose");
+        return Err("bad path purpose".into());
     }
     if derivation_path.path()[1].to_u32() != 540 {
-        err!("bad path coin type");
+        return Err("bad path coin type".into());
     }
     for p in derivation_path.path() {
         if !p.is_hardened() {
-            err!("path isn't fully hardened");
+            return Err("path isn't fully hardened".into());
         }
     }
 
     let extended = ExtendedSecretKey::from_seed(seed_slice)
-        .and_then(|extended| extended.derive(&derivation_path));
-    check_err!(extended, "failed to derive secret key from seed");
-    let extended_inner = extended.unwrap();
-    let extended_public_key = extended_inner.public_key();
-    let keypair = Keypair{secret: extended_inner.secret_key, public: extended_public_key};
-    let result_slice = unsafe { std::slice::from_raw_parts_mut(result, KEYPAIR_LENGTH) };
-    result_slice.copy_from_slice(&keypair.to_bytes());
-    0
+        .map_err(|e| format!("failed to derive secret key from seed: {}", e))?
+        .derive(&derivation_path)
+        .map_err(|e| format!("failed to derive derivation path: {}", e))?;
+    let extended_public_key = extended.public_key();
+    Ok(Keypair{secret: extended.secret_key, public: extended_public_key})
 }
